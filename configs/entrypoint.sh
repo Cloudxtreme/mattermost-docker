25f225078e8dc4e8fa94a02cb9fc6d16f7703053
Bart Bania <contact@bartbania.com>
2016-03-08 11:17:45 +0000

    postgres

--- configs/entrypoint.sh
+++ configs/entrypoint.sh
@@ -7,6 +7,12 @@ set -e
 : ${PG_DATA:=$DB_ENV_POSTGRES_DB}
 export PG_HOST PG_USER PG_PASS PG_DATA
 
+sed -i 's/PG_HOST/${PG_HOST}/g' /mattermost_data/config/config.json
+sed -i 's/PG_USER/${PG_USER}/g' /mattermost_data/config/config.json
+sed -i 's/PG_PASS/${PG_PASS}/g' /mattermost_data/config/config.json
+sed -i 's/PG_DATA/${PG_DATA}/g' /mattermost_data/config/config.json
+sed -i '4,15d' /entrypoint.sh
+
 if [ "${1:0:1}" = '-' ]; then
   set -- mattermost "$@"
 fi
