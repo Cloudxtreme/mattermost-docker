0261be96c4548de3aa5d893a173ac1e51ba052c6
Bart Bania <contact@bartbania.com>
2016-03-08 08:51:46 +0000

    database_variables

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 FROM bbania/centos:base
 MAINTAINER "Bart Bania" <contact@bartbania.com>
 
-ENV PG_USER=mattermost
-ENV PG_PASS=mattermost
-ENV PG_HOST=postgres
-ENV PG_DATA=mattermost
-
 RUN yum install -q -y python-pip && \
     yum -q clean all
 RUN pip -q install supervisor
--- configs/entrypoint.sh
+++ configs/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+: ${PG_HOST:=$DB_PORT_5432_TCP_ADDR}
+: ${PG_USER:=${DB_ENV_POSTGRES_USER:='postgres'}}
+: ${PG_PASS:=$DB_ENV_POSTGRES_PASSWORD}
+: ${PG_DATA:=$DB_ENV_POSTGRES_DB}
+export PG_HOST PG_USER PG_PASS PG_DATA
+
 if [ "${1:0:1}" = '-' ]; then
   set -- mattermost "$@"
 fi
